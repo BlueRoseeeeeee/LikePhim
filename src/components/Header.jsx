79a488d02578399ef26fa9682f87ca55d980f5da
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-06-15 08:01:05 +0700

    fix conflict tại Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,19 +15,6 @@ const Header = ({search}) => {
           <a href='' className='text-white text-base md:text-lg'>Home</a>
           <a href='' className='text-white text-base md:text-lg'>About</a>
           <a href='' className='text-white text-base md:text-lg'>Contact</a>
-=======
-    <div className="p-4 bg-black flex items-center justify-between">
-    <div className="flex items-center space-x-4">
-      <div className=' flex items-center flex-row'>
-        <h1 className='text-[35px] font-bold text-white'>LIKE</h1>
-        <h1 className='text-[35px] font-bold text-blue-500'>PHIM</h1>
-      </div>
-        <nav className='flex items-center space-x-4'>
-        <a href='https://thichxemphim.vercel.app/' className='text-white'>Home</a>
-        <a href='' className='text-white'>About</a>
-        <a href='' className='text-white'>Contact</a>
-
->>>>>>> 4375148c47e4772e01e7d49c0d961d1698fd4f88
         </nav>
       </div>
       <div className='flex items-center space-x-2 md:space-x-4 w-full md:w-auto'>
